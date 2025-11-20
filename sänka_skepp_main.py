08b4fff35a3c7dcd0db7cc043ae710a0e60309b3
Tomas Forssmark <tpa1887@gmail.com>
2025-11-20 20:28:05 +0100

    GUI

--- sänka_skepp_main.py
+++ sänka_skepp_main.py
@@ -1,40 +1,41 @@
 #Har använt mig av chatgpt för att hitta metoder att komma vidare när jag fastnat!
- 
+
 from felhantering_sanka_skepp import Gilltigta_mål
 import slumpgenerator
 import Highscore
 import sys
 class Bräde:
     def __init__(self):
-        
+
         slump_bräde = slumpgenerator.Slumpande()
         self.spelplan,self.spelplan_fullsänkt=slump_bräde.slumpgenerator()
         self.sänkta_fartyg=[]
         self.missade_fartyg=[]
         self.highscore_länkning=Highscore.Highscore_räknare()
     def kontroll(self):
-        while True:
+        if skott_val in self.spelplan and skott_val not in self.sänkta_fartyg and skott_val not in self.missade_fartyg:
+            print("Träff! ")
+            return True , skott_val
+        elif skott_val in self.sänkta_fartyg or skott_val in self.missade_fartyg:
+            print("Du har redan skjutigt där! ")
+            return False, skott_val
+        else:
+            print("Miss! ")
+            return False, skott_val
+
+
+
+
+    def skjuta(self):
+
+        while(not skott_granskning):
             skott_val=input("Vad vill du skjuta? ").upper()
             skott_granskning=Gilltigta_mål(skott_val)
-            if skott_granskning==True:
-                if skott_val in self.spelplan and skott_val not in self.sänkta_fartyg and skott_val not in self.missade_fartyg:
-                    print("Träff! ")
-                    return True , skott_val
-                elif skott_val in self.sänkta_fartyg or skott_val in self.missade_fartyg:
-                    print("Du har redan skjutigt där! ")
-                    continue
-                else:
-                    print("Miss! ")
-                    return False, skott_val
-            elif skott_granskning==False:
-                continue
-   
-   
-   
-    def skjuta(self):
-         
+            
+
+
         resultat, träff_eller_miss=self.kontroll()
-        if resultat==True:    
+        if resultat==True:
             self.spelplan.remove(träff_eller_miss)
             self.sänkta_fartyg.append(träff_eller_miss)
             for innre_lista in self.spelplan_fullsänkt:
@@ -52,7 +53,7 @@ class Bräde:
             self.missade_fartyg.append(träff_eller_miss)
             self.highscore_länkning.räknare()
         self.rita_bräde()
-            
+
     def rita_bräde(self,extra=False):
         self.missade_fartyg
         self.sänkta_fartyg
@@ -84,8 +85,8 @@ class Bräde:
                 bokstav_plus=chr(ord(bokstav) + 1)
                 siffra_minus=siffra-1
                 siffra_plus=siffra+1
-                self.missade_fartyg.extend([x for x in[f"{bokstav_minus}{siffra_minus}",f"{bokstav_minus}{siffra}",f"{bokstav_minus}{siffra_plus}",f"{bokstav}{siffra_minus}",f"{bokstav}{siffra_plus}",f"{bokstav_plus}{siffra_minus}",f"{bokstav_plus}{siffra}",f"{bokstav_plus}{siffra_plus}"]if x not in self.sänkta_fartyg]) 
-                
+                self.missade_fartyg.extend([x for x in[f"{bokstav_minus}{siffra_minus}",f"{bokstav_minus}{siffra}",f"{bokstav_minus}{siffra_plus}",f"{bokstav}{siffra_minus}",f"{bokstav}{siffra_plus}",f"{bokstav_plus}{siffra_minus}",f"{bokstav_plus}{siffra}",f"{bokstav_plus}{siffra_plus}"]if x not in self.sänkta_fartyg])
+
 
 
 def main():
@@ -100,7 +101,7 @@ def main():
         elif meny_val=="3":
             print("Programmet Avslutar.....")
             databas.rita_bräde(extra=True)
-            sys.exit() 
+            sys.exit()
         else:
             print("Du måste ange ett heltal 1,2,3")
-main()
+#main()
